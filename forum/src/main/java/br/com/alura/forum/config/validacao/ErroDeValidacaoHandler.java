1f45d9401d733c7e7077debee5c4968de5ded714
Mateus Valentim de Oliveira <mateus.oliveira@paygo.corp>
2021-06-24 15:33:18 -0300

    finalização do handler

--- forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
+++ forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -1,6 +1,15 @@
 package br.com.alura.forum.config.validacao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.hibernate.jdbc.Expectation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,9 +18,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
 
+	//MessageSource responsavel pela internacionalização
+	@Autowired
+	private MessageSource messageSource;
+	
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public List<ErroDeFormularioDto> handle(MethodArgumentNotValidException exception){
+	public List<ErroDeFormularioDto> handle(MethodArgumentNotValidException exception) {
+		List<ErroDeFormularioDto> dto = new ArrayList<>();
+
+		List<FieldError> fielErrors =  (List<FieldError>) exception.getBindingResult().getFieldError();
+		fielErrors.forEach(e -> {
+			String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+			ErroDeFormularioDto erro = new ErroDeFormularioDto(e.getField(), mensagem);
+			dto.add(erro);
+		});
 		
+		return dto;
 	}
 }
