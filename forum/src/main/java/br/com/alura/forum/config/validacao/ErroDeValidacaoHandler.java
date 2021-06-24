7b585f7b4b7c74fe2f86fd5df5434a42257e7b71
Mateus Valentim de Oliveira <mateus.oliveira@paygo.corp>
2021-06-24 13:43:32 -0300

    criando pacote config.validacao e classe ErroDeValidacaoHandler com a anotaçao RestControllerAdvice

---
+++ forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,10 @@
+package br.com.alura.forum.config.validacao;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+	
+}
