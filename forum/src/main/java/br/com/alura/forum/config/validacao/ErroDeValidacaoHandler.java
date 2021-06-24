bf4436a2b2d20f19d5e816c5cebb4d2f6fab7ebb
Mateus Valentim de Oliveira <mateus.oliveira@paygo.corp>
2021-06-24 13:46:35 -0300

    criando método handle na classe ErroDeValidacao

--- forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
+++ forum/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -1,10 +1,17 @@
 package br.com.alura.forum.config.validacao;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
 
-	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<ErroDeFormulario> handle(MethodArgumentNotValidException exception){
+		
+	}
 }
